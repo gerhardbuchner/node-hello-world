d80beb19b791d5fc8b191c3453063913da18bbbd
gerhardbuchner <gerhard@keysfornow.com>
2020-11-03 16:53:04 +0200

    first commit

--- backup1/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-let hitcount = 0;
-http.createServer((request, response) => {
-    hitcount += 1;
-    console.log("Hello World "+hitcount);
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end("Hello World "+hitcount, 'utf-8');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
